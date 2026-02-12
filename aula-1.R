7fb7cab6894920635379b28c830057556565ebef
Alan Echer <alan.echer@gmail.com>
2026-02-12 18:23:36 -0300

    aula 1

---
+++ aula-1.R
@@ -0,0 +1,31 @@
+#Primeiro comando
+print("hello world!")
+
+# R como calculadora ------------------------------------------------------
+2 + 2
+4^2
+sqrt(16)
+log(3)
+4 / 5
+4 %% 5
+print(pi)
+
+
+# Instalação de pacotes ---------------------------------------------------
+install.packages("tidyverse")
+
+# Carrega o pacote --------------------------------------------------------
+library(tidyverse)
+
+# Pedido Ajuda ------------------------------------------------------------
+help(sqrt)
+help(round)
+?(sqrt)
+
+x <- c(0:10, 50)
+x
+mean(x)
+sum(x)
+sqrt(x)
+
+y = sqrt(x)
